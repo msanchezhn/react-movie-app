0dfb1798a7ad6ce568cffbeb14f96badc2f28a5e
ZuRdO <mairon.fcbarca10@gmail.com>
2021-10-20 21:13:25 -0600

    update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return (
         <div className="footer">
             <div>Movie App</div>
-            <div>©2021, Mairon Sanchez</div>
+            <div>©2021, Mairon Sanchez in Github</div>
         </div>
     );
 };
